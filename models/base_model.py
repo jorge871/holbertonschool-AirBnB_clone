ef2493c1d896f2197ca66919be7e6212ec4f325b
jorge871 <jg303114@gmail.com>
2024-02-28 17:58:01 +0000

    updated classes

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,18 @@ class BaseModel():
     """create class that will inherited by the other classes in this project"""
     def __init__(self, *args, **kwargs):
         """Initialize new basemodel"""
-    
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
 
     def __str__(self):
         """ this is a representation of string"""
-        return ("[{}] ({}) <{}>".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[<{}>] (<{}>) <{}>".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-
-    def save(self):
-        """updates the public instance attribute created_at"""
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
@@ -32,7 +27,7 @@ class BaseModel():
             "my_number": self.my_number,
             "name": self.name,
             "__class__": self.__class__.__name__,
-            "updated_at": self.update_at,
+            "updated_at": self.updated_at,
             "id": self.id,
             "created_at": self.created_at
         }

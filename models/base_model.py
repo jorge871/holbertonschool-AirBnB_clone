fa06e48feea5f378d1b363f908f3ff8f89279844
jorge871 <jg303114@gmail.com>
2024-02-28 17:43:21 +0000

    updated classes

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,11 @@ class BaseModel():
 
     def save(self):
         """updates the public instance attribute updated_at"""
-        self.update_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+    def save(self):
+        """updates the public instance attribute created_at"""
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""

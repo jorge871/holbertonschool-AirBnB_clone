0cf206bd57068d6c2b45f9666651dfa901171a27
jorge871 <jg303114@gmail.com>
2024-02-28 17:41:17 +0000

    updated classes

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,33 @@
 """Created class for this project"""
 from datetime import datetime
 import uuid
-import models
 
 
 class BaseModel():
     """create class that will inherited by the other classes in this project"""
     def __init__(self, *args, **kwargs):
         """Initialize new basemodel"""
+    
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
-        self.id = uuid
-        self.created_at = datetime
-        self.updated_at = datetime
+
+    def __str__(self):
+        """ this is a representation of string"""
+        return ("[{}] ({}) <{}>".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute updated_at"""
-        self.update_at = datetime
+        self.update_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
+        return {
+            "my_number": self.my_number,
+            "name": self.name,
+            "__class__": self.__class__.__name__,
+            "updated_at": self.update_at,
+            "id": self.id,
+            "created_at": self.created_at
+        }

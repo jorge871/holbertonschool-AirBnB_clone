46969c284c2365e41f958959f750c0a28882efff
jorge871 <jg303114@gmail.com>
2024-02-28 17:44:56 +0000

    updated classes

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel():
         """Initialize new basemodel"""
     
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = datetime.now("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now("%Y-%m-%dT%H:%M:%S.%f")
 
 
     def __str__(self):

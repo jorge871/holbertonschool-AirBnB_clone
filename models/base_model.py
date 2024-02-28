d230c52facb9ff8eb304b03ece474fefea95f798
Daldanos <adolfogsilveira@gmail.com>
2024-02-28 06:24:59 -0800

    Making changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """Created class for this project"""
-import datetime
+from datetime import datetime
 import uuid
 import models
 
 class BaseModel():
     """create class that will be inherited by the other classes in this project"""
-    pass
+    def __init__(self, *args, **kwargs):
+        """Initialize new basemodel"""
+
+        self.id = 
+        self.created_at = datetime
+        self.updated_at = datetime
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+        self.update_at = datetime
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+

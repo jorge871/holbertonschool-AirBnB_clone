678442495ea80d4c53b12ba7469568b7bf8f5a31
Daldanos <adolfogsilveira@gmail.com>
2024-02-28 06:55:49 -0800

    Making changes

---
+++ console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""console for AirBNB clone"""
+import cmd
+
+class Console(cmd.Cmd):
+    """create the console prompt"""
+    prompt = "(hbnb) "
+    pass
+
+if __name__ == "__main__":
+        Console().cmdloop()

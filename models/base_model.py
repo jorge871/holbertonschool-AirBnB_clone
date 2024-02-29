7541f353e630116359432b2c220f3419d44b3046
Daldanos <adolfogsilveira@gmail.com>
2024-02-28 19:15:30 -0800

    Making changes

--- models/base_model.py
+++ models/base_model.py
@@ -24,6 +24,7 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""

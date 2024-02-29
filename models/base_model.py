2e964a39c53f7d32ba712e998a75570be043b91f
Daldanos <adolfogsilveira@gmail.com>
2024-02-28 19:11:05 -0800

    Making changes

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Initialize new basemodel"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
 
     def __str__(self):
@@ -23,11 +23,11 @@ class BaseModel():
 
     def save(self):
         """updates the public instance attribute updated_at"""
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
-        data = self.__dict__copy()
+        data = self.__dict__.copy()
         data["created_at"] = self.created_at.isoformat()
         data["updated_at"] = self.updated_at.isoformat() 
         data["__class__"] = self.__class__.__name__

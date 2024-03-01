e8d5802c37110a3e3830e25d6dfa9fb338a8219c
jorge871 <jg303114@gmail.com>
2024-03-01 03:56:58 -0300

    updated file storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 """ create file storage class """
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.user import User
-from models.state import State
-from models.review import Review
+import json
 
 
 class FileStorage():
-    """this is the class to handle the file operations of our interpreter"""
-    pass
+    """this is the class to handle the file operations of our
+    interpreter"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """return a dictionary with all objects in memory"""
+        return self.__objects
+
+    def new(self, obj):
+        """Assigns to the object in the '__objects' set the value
+        specified by the key '<object class name>.id'"""
+        self.__objects [f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        """serialized a objects to file json"""
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            json.dump({key: value.to_dict() if hasattr(value, 'to_dict') else value for key, value in self.__objects.items()}, file)
+
+    def reload(self):
+        """deserializes a objects to file json"""
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                self.__objects = json.load(file)
+        except Exception:
+            pass

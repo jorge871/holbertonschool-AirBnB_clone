b6978a9a002926430ccf3d562f6ee3c3a1ea7f0e
Daldanos <adolfogsilveira@gmail.com>
2024-03-01 18:07:01 -0800

    Making changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """ create file storage class """
 import json
 from models.base_model import BaseModel
@@ -22,25 +22,22 @@ class FileStorage():
         """Assigns to the object in the '__objects' set the value
         specified by the key '<object class name>.id'"""
         if obj is not None:
-            """FileStorage.__objects[obj.id] = obj"""
-        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+            FileStorage.__objects[obj.id] = obj
+        """FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj"""
 
     def save(self):
         """serialized a objects to file json"""
-        with open(self.__file_path, "w", encoding="utf-8") as f:
+        with open(self.__file_path, "w") as f:
             json.dump({key: value.to_dict() for key, value in self.__objects.items()}, f)
             """json.dump({key: value.to_dict() if hasattr(value, 'to_dict') else value for key, value in self.__objects.items()}, f)"""
 
     def reload(self):
-        """Deserializes objects from a JSON file"""
+        """deserializes a objects to file json"""
         try:
-            with open(self.__file_path, "r+", encoding="utf-8") as f:
-                self.__objects = {}
+            with open(self.__file_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
-                for key in data.keys():
-                    cls_name = data[key].pop("__class__", None)
-                    created_at = data[key].pop("created_at", None)
-                    updated_at = data[key].pop("updated_at", None)
-                    self.__objects[key] = eval(f"{cls_name}(**data[key])")
+                for key, value in data.items():
+                    obj_class = value["__class__"]
+                    self.__objects[key] = eval(obj_class)(**value)
         except Exception:
             pass
